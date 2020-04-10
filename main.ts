4ac246164a0c04258be944109b326a56135d6b74
george.orhewere@gmail.com <george.orhewere@gmail.com>
2020-04-10 11:18:29 +0100

    Implemented observable using the create method.

--- main.ts
+++ main.ts
@@ -3,7 +3,19 @@ import { Observable,of, Observer } from 'rxjs';
 var programmingLanguages = ["Java","C#","Python","Javascript"];
 
 //Observable.from is depreciated
-var source = of("Java","C#","Python","Javascript");
+// var source = of("Java","C#","Python","Javascript");
+var source = Observable.create(observer =>{
+
+    programmingLanguages.forEach(element => {
+        if(element === "Python"){
+            observer.error("Something broke my code");
+        }
+        observer.next(element)
+    });
+
+    observer.complete()
+
+});
 
 
 source.subscribe(
